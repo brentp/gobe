43ca96ff6108e0a5052f9a1ba738cb5f468bac41
brentp <bpederse@gmail.com>
2010-08-02 11:16:24 -0700

    only show stuff with a title in the history, and dont add a title automatically.

--- try-gobe.appspot.com/main.py
+++ try-gobe.appspot.com/main.py
@@ -35,9 +35,10 @@ class Index(webapp.RequestHandler):
             a = Annotation.all().filter('anno_id = ', anno_id).get()
         if a is None: a = Annotation()
         user_tmpl = user_stuff(self.request.uri)
-        history = Annotation.all().order("-date").fetch(20)
+        history = [x for x in Annotation.all().order("-date") if x.title][:12]
         name = user and user.nickname() or ""
-        user_history = Annotation.gql("WHERE author = :author", author=user).fetch(20) if user else None
+        user_history = [x for x in Annotation.gql("WHERE author = :author", \
+                              author=user) if x.title][:12] if user else None
 
         bar_hists = a.get_bar_hists()
 
@@ -49,24 +50,22 @@ class Index(webapp.RequestHandler):
     def post(self, unused):
         self.response.headers['Content-type'] = 'text/javascript';
         user = users.get_current_user()
-        import sys
         annos = self.request.get('annos', '').strip()
         if not annos:
             simplejson.dumps({'status': 'fail'})
             return
         name = user and user.nickname() or ""
-        print >>sys.stderr, name
-        title = self.request.get('title') or name + "|" + str(datetime.datetime.now())
-        id = hashlib.md5(annos + title).hexdigest()
+        title = self.request.get('title') or ""
+        anno_id = hashlib.md5(annos + title).hexdigest()
 
         # dont save unless it's new.
-        a = Annotation.gql("WHERE anno_id = :id AND title = :title",
-                           id=id, title=title).get()
+        a = Annotation.gql("WHERE anno_id = :anno_id AND title = :title",
+                           anno_id=anno_id, title=title).get()
         if a is None:
-            a = Annotation(title=title, author=user, content=annos.strip(), anno_id=id)
+            a = Annotation(title=title, author=user, content=annos.strip(), anno_id=anno_id)
             a.put()
 
-        self.response.out.write(simplejson.dumps({'status':'success', 'anno_id': id}))
+        self.response.out.write(simplejson.dumps({'status':'success', 'anno_id': anno_id}))
 
 class Anno(webapp.RequestHandler):
     def get(self, anno_id):
